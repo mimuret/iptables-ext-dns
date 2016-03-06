0fa2cd291dec0571edf15d9b11aac20379945fb9
mimuret <mimuret@gmail.com>
2016-03-06 21:12:00 +0900

    update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,41 +1,45 @@
 #!/bin/sh
 
 KERNEL_RELEASE=`uname -r`
-LDNS_DEVEL=''
 
 if [ -e '/etc/centos-release' ] ; then
+        IPT_VERSION=$(rpm -qi iptables  | grep Version| awk '{print $3}')
+
+        PACKAGES="gcc make automake libtool"
+        PACKAGES="$PACKAGES kernel-headers-$KERNEL_RELEASE kernel-devel-$KERNEL_RELEASE"
+        PACKAGES="$PACKAGES iptables-devel-$IPT_VERSION"
+
         if [ "$1" = "--debug" ] ; then
-                yum install -y bind-utils ldns nc vim-common
+          PACKAGES="$PACKAGES bind-utils ldns-utils nc vim-common"
         fi
-        yum install -y gcc \
-                make automake libtool \
-                iptables-devel \
-                kernel-headers-$KERNEL_RELEASE \
-                kernel-devel-$KERNEL_RELEASE
+        yum install -y $PACKAGES
         exit $?
 fi
 if [ -e '/etc/fedora-release' ] ; then
+        IPT_VERSION=$(rpm -qi iptables  | grep Version| awk '{print $3}')
+        
+        PACKAGES="gcc make automake libtool"
+        PACKAGES="$PACKAGES kernel-headers-$KERNEL_RELEASE kernel-devel-$KERNEL_RELEASE"
+        PACKAGES="$PACKAGES iptables-devel-$IPT_VERSION"
+        
         if [ "$1" = "--debug" ] ; then
-          yum install -y bind-utils ldns-utils nc vim-common
+          PACKAGES="$PACKAGES bind-utils ldns-utils nc vim-common"
         fi
-        dnf install -y gcc \
-                make automake libtool \
-                iptables-devel \
-                kernel-headers-$KERNEL_RELEASE \
-                kernel-devel-$KERNEL_RELEASE
+
+        dnf install $PACKAGES
         exit $?
 fi
 if [ -e '/etc/debian_version' ] ; then
+        IPT_VERSION=$(apt show iptables | grep 'Version' | awk '{print $2}')
+
+        PACKAGES="gcc make automake libtool"
+        PACKAGES="$PACKAGES linux-headers=$KERNEL_RELEASE"
+        PACKAGES="$PACKAGES iptables-dev=$IPT_VERSION"
+
         if [ "$1" = "--debug" ] ; then
-                apt-get install -y dnsutils ldnsutils
+          PACKAGES="$PACKAGES dnsutils ldnsutils"
         fi
-        apt-get install -y gcc \
-                make \
-                automake \
-                autoconf \
-                libtool \
-                linux-headers-$KERNEL_RELEASE \
-                iptables-dev
+        apt-get install -y $PACKAGES
         exit $?
 fi
 
