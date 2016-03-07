de45924aadfc2440a3ef3fe8e750d500e6be2117
mimuret <mimuret@gmail.com>
2016-03-07 20:28:18 +0900

    Update install-dependencies.sh for RH

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 KERNEL_RELEASE=`uname -r`
+LANG=C
+
+export LANG
 
 if [ -e '/etc/redhat-release' ] ; then
-        IPT_VERSION=$(rpm -qi iptables  | grep Version| awk '{print $3}')
-
-        PACKAGES="gcc make automake libtool"
-        PACKAGES="$PACKAGES kernel-headers-$KERNEL_RELEASE kernel-devel-$KERNEL_RELEASE"
-        PACKAGES="$PACKAGES iptables-devel-$IPT_VERSION"
-
-        exit $?
-fi
-if [ -e '/etc/fedora-release' ] ; then
+        which dnf > /dev/null
+        if [ "$?" = "0" ] ; then
+          PKG='dnf'
+        else
+          PKG='yum'
+        fi
         IPT_VERSION=$(rpm -qi iptables  | grep Version| awk '{print $3}')
         
         PACKAGES="gcc make automake libtool"
@@ -22,12 +22,16 @@ if [ -e '/etc/fedora-release' ] ; then
           PACKAGES="$PACKAGES bind-utils ldns-utils nc vim-common"
         fi
 
-        dnf install $PACKAGES
+        $PKG install -y $PACKAGES
         exit $?
 fi
 if [ -e '/etc/debian_version' ] ; then
-        IPT_VERSION=$(aptitude show iptables | grep 'Version' | awk '{print $2}')
-
+        which apt > /dev/null
+        if [ "$?" = "0" ] ; then
+          IPT_VERSION=$(apt show iptables | grep 'Version' | awk '{print $2}')
+        else
+          IPT_VERSION=$(aptitude show iptables | grep 'Version' | awk '{print $2}')
+        fi
         PACKAGES="gcc make automake libtool"
         PACKAGES="$PACKAGES linux-headers-$KERNEL_RELEASE"
         PACKAGES="$PACKAGES iptables-dev=$IPT_VERSION"
